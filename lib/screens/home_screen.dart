38286a677afb69b67662eb555847ed487fbdc5a8
Reza Eftekhar <reza.eftekhar80@gmail.com>
2025-10-25 20:01:28 -0400

    customize Texts

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_userside/providers/theme_provider.dart';
+import 'package:shop_userside/widgets/subtitle_text.dart';
+import 'package:shop_userside/widgets/title_text.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,17 +16,16 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
 
           children: [
-            Text(
-              'Hello world',
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            ),
+            SubtitleTextWidget(label: 'Hello ', fontSize: 50),
+            TitleTextWidget(label: 'Hello title'),
             ElevatedButton(onPressed: () {}, child: Text('Hello world')),
             SwitchListTile(
-              title: Text(themeProvider.getDarkTheme?'Dark Mode':'Light Mode'),
+              title: Text(
+                themeProvider.getDarkTheme ? 'Dark Mode' : 'Light Mode',
+              ),
               value: themeProvider.getDarkTheme,
               onChanged: (value) {
                 themeProvider.setDarkTheme(value);
-                print('kkkkkkkkkkkkkkkkk${themeProvider.getDarkTheme}');
               },
             ),
           ],
